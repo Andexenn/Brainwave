b6bbb0f474aec16a9a29013d4185b54cb438de86
ryan <phanhuynhhung0707@gmail.com>
2025-07-08 22:02:08 +0700

    2. finish the button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
- 
+import React from "react";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+import Header from "./components/Header";
+
 const App = () => {
   return (
-    <h1></h1>
-  )
-}
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10">
+          Something
+        </Button>
+        <Header />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
 
-export default App
+export default App;
